c9f5d014c23dd4ee8b647cea11842b73f1319d2b
maksim.sorokin <SorokinMaksimArtemovich@yandex.ru>
2020-04-07 21:47:25 +0300

    Add file

---
+++ PolynomialWithFractions/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+
+namespace lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            rational_fraction fraction;
+            set_of_fractions set = null;
+            int num, denum, first_lenth, second_lenth;
+            bool is_number = Int32.TryParse(args[0], out first_lenth);
+            if (is_number)
+            {
+                for (int i = 1; i < first_lenth * 2 + 1; i += 2)
+                {
+                    is_number = Int32.TryParse(args[i], out num);
+                    is_number &= Int32.TryParse(args[i + 1], out denum);
+                    if (is_number && (denum > 0))
+                    {
+                        fraction = new rational_fraction(num, (uint)denum);
+                        if (set == null)
+                        {
+                            set = new set_of_fractions(fraction);
+                            Console.Write("\n");
+                            Console.Write(fraction.value);
+                        }
+                        else
+                        {
+                            set.add(fraction);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fraction {0} is invalid", i);
+                    }
+                }
+            }
+            if (set != null)
+            {
+                Console.Write("\n");
+                Console.Write(set.min.value);
+
+                Console.Write("\n");
+                double doub;
+                is_number = Double.TryParse(args[first_lenth * 2 + 1], out doub);
+                Console.Write(set.more_then(doub));
+                polynomial polly = new polynomial();
+                polly.get(set);
+                Console.Write("\n");
+                Console.Write(polly.return_string());
+                set = null;
+                is_number = Int32.TryParse(args[first_lenth * 2 + 2], out second_lenth);
+                if (is_number)
+                {
+                    for (int i = first_lenth * 2 + 3; i < first_lenth * 2 + 3 + second_lenth * 2; i += 2)
+                    {
+                        is_number = Int32.TryParse(args[i], out num);
+                        is_number &= Int32.TryParse(args[i + 1], out denum);
+                        if (is_number && (denum > 0))
+                        {
+                             fraction = new rational_fraction(num, (uint)denum);
+                            if (set == null)
+                            {
+                                set = new set_of_fractions(fraction);
+                            }
+                            else
+                            {
+                                set.add(fraction);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fraction {0} is invalid", i);
+                        }
+                    }
+                    polynomial polly_2 = new polynomial();
+                    polly_2.get(set);
+                    polly.sum(polly_2);
+                    Console.Write("\n");
+                    Console.Write(polly.return_string());
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PolynomialWithFractions/polynomial.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_2
+{
+    class polynomial
+    {
+        private set_of_fractions coefficients;
+        public void get(set_of_fractions k)
+        {
+            coefficients = new set_of_fractions(k);
+        }
+        public string return_string()
+        {
+            string made_string = "";
+            for (int i = 0; i < this.coefficients.count; i++)
+            {
+                if (coefficients.fractions[i].numerator != 0)
+                {
+                    made_string += this.coefficients.fractions[i].numerator.ToString();
+                    made_string += '/';
+                    made_string += this.coefficients.fractions[i].denumerator.ToString();
+
+                    if (this.coefficients.count - i > 2)
+                    {
+                        made_string += " x^";
+                        made_string += (this.coefficients.count - i - 1);
+                        made_string += " + ";
+                    }
+                    else if (this.coefficients.count - i == 2)
+                    {
+                        made_string += " x + ";
+                    }
+                }
+            }
+            return made_string;
+        }
+        public void sum(polynomial polly)
+        {
+            this.coefficients.sum(polly.coefficients);
+        }
+    }
+}
